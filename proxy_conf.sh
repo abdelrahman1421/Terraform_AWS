daa7f782ed9e1bc08344b374c045bd156abc0327
Abdelrahman Mohamed Roshdy <abdohamoda636@gmail.com>
2022-10-18 22:58:43 +0200

    Configure Backend on AWS

--- proxy_conf.sh
+++ proxy_conf.sh
@@ -5,7 +5,7 @@ sudo echo "server {
 listen 80;
 server_name _;
 location / {
-proxy_pass http://internal-terraform-private-load-balancer-855477777.us-east-1.elb.amazonaws.com;
+proxy_pass http://dns_name
 }
 }" > /etc/nginx/sites-available/custom_server.conf
 sudo ln -s /etc/nginx/sites-available/custom_server.conf /etc/nginx/sites-enabled/custom_server.conf
